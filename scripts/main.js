e0e8b3e789e1154c22bed5d31d8148115f78905c
Dick Leung <yhd.leung@gmail.com>
2022-11-14 23:12:00 +0800

    fix: shift launchpad button position to the right of planet selection button

--- scripts/main.js
+++ scripts/main.js
@@ -60,7 +60,7 @@ const addSelectionButton = (callback) => {
       if (sectors.length) {
         Vars.ui.planet.fill(
           cons((t) => {
-            t.top().left().marginTop(5).marginLeft(5).defaults().size(200, 54);
+            t.top().left().marginTop(5).marginLeft(210).defaults().size(200, 54);
             t.button(
               "Launchpads",
               new TextureRegionDrawable(
